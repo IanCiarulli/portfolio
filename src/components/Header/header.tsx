042f3002ef2bc144f7dad9183596dc6f05d41eaa
Ian Ciarulli <ian.ciarulli@gmail.com>
2025-05-02 17:44:11 -0300

    about me v1

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -3,10 +3,10 @@ import { useLanguage } from '../../context/LanguageContext';
 export function Header() {
   const { lang, toggleLang } = useLanguage();
   return (
-    <header className="fixed top-0 right-0 left-0 z-50 font-mono text-sm backdrop-blur-sm">
+    <header className="fixed top-0 right-0 left-0 z-50 text-sm backdrop-blur-sm">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4 md:px-8">
         <h1 className="after:bg-primary relative cursor-pointer text-sm font-bold after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:transition-all after:duration-600 hover:after:w-full">
-          <a href="#">Ian Ciarulli</a>
+          Ian Ciarulli
         </h1>
         <nav className="hidden items-center space-x-6 text-white md:flex">
           <a
