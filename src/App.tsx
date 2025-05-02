954c815660d4847066c082393d50fb77eee1fa5d
Ian Ciarulli <ian.ciarulli@gmail.com>
2025-05-01 22:31:55 -0300

    v1 header done

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <LanguageProvider>
       <div className="flex h-full w-full items-center justify-center">
-        <div className="flex h-full flex-col items-center bg-gray-600 p-4 pt-30 sm:w-full md:w-3/5 lg:w-3/5">
-          <Header />
+        <Header />
+        <div className="flex h-full flex-col items-center p-4 pt-30 sm:w-full md:w-3/5 lg:w-3/5">
           <p className="mt-1 text-black">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis,
             at, atque eligendi, maxime velit magni minus perferendis accusamus
--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -3,34 +3,45 @@ import { useLanguage } from '../../context/LanguageContext';
 export function Header() {
   const { lang, toggleLang } = useLanguage();
   return (
-    <header className="fixed top-0 right-0 left-0 z-50 font-mono text-xs backdrop-blur-sm">
+    <header className="fixed top-0 right-0 left-0 z-50 font-mono text-sm backdrop-blur-sm">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4 md:px-8">
-        <h1 className="text-sm font-bold">Ian Ciarulli</h1>
+        <h1 className="after:bg-primary relative cursor-pointer text-sm font-bold after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:transition-all after:duration-600 hover:after:w-full">
+          <a href="#">Ian Ciarulli</a>
+        </h1>
         <nav className="hidden items-center space-x-6 text-white md:flex">
-          <a href="#about" className="transition-colors hover:text-teal-400">
+          <a
+            href="#about"
+            className="hover:text-primary transition-colors hover:scale-115"
+          >
             {lang === 'EN' ? 'About Me' : 'Sobre mi'}
           </a>
-          <a href="#work" className="transition-colors hover:text-teal-400">
+          <a
+            href="#work"
+            className="hover:text-primary transition-colors hover:scale-115"
+          >
             {lang === 'EN' ? 'Experience' : 'Experiencia'}
           </a>
-          <a href="#projects" className="transition-colors hover:text-teal-400">
+          <a
+            href="#projects"
+            className="hover:text-primary transition-colors hover:scale-115"
+          >
             {lang === 'EN' ? 'My work' : 'Mis trabajos'}
           </a>
         </nav>
-        <div className="flex w-auto items-center justify-between">
-          <label className="relative inline-flex cursor-pointer items-center select-none">
-            <div className="flex h-6 w-18 rounded-md bg-gray-200">
+        <div className="flex w-auto items-center">
+          <label className="relative inline-flex cursor-pointer select-none">
+            <div className="flex h-6 w-18 items-center justify-center font-bold">
               <input
                 type="checkbox"
                 className="peer sr-only"
                 checked={lang === 'ES'}
                 onChange={toggleLang}
               />
-              <span className="absolute top-0 left-0 h-full w-1/2 rounded-sm bg-blue-500 transition-transform peer-checked:translate-x-full"></span>
-              <span className="relative z-10 flex-1 text-center leading-8 text-white peer-checked:text-gray-700">
+              <span className="absolute top-0 left-0 h-full w-1/2 rounded-xs border border-gray-300 transition-transform duration-500 peer-checked:translate-x-full"></span>
+              <span className="text-primary relative z-10 flex-1 text-center peer-checked:text-white">
                 EN
               </span>
-              <span className="relative z-10 flex-1 text-center leading-8 text-gray-700 peer-checked:text-white">
+              <span className="peer-checked:text-primary relative z-10 flex-1 text-center text-white">
                 ES
               </span>
             </div>
