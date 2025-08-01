ce01c3a80c51ae05422c655fa8303f29c53d5f37
Ian Ciarulli <ian.ciarulli@gmail.com>
2025-07-31 22:15:14 -0300

    hero font fixes and cta text change

--- src/components/CvCta/cvCta.tsx
+++ src/components/CvCta/cvCta.tsx
@@ -7,7 +7,7 @@ export const CvCta = () => {
       download="IanCiarulli_CV.pdf"
       className="arrow-container group bg-cta text-cta-text hover:bg-cta-hover inline-flex items-center gap-2 overflow-hidden rounded-md px-5 py-2 text-sm font-semibold shadow transition-all sm:text-base"
     >
-      Download CV
+      Download Resume
     </a>
   );
 };
