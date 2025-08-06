74fc93b250b6917b7b5427498f84622cbc4f65c9
Ian Ciarulli <ian.ciarulli@gmail.com>
2025-08-05 22:40:27 -0300

    fix: cvcta height fix

--- src/components/CvCta/cvCta.tsx
+++ src/components/CvCta/cvCta.tsx
@@ -5,7 +5,7 @@ export const CvCta = () => {
     <a
       href="/files/test.pdf"
       download="IanCiarulli_CV.pdf"
-      className="arrow-container group bg-cta text-cta-text hover:bg-cta-hover inline-flex items-center gap-2 overflow-hidden rounded-md px-5 py-2 text-sm font-semibold shadow transition-all sm:text-base"
+      className="arrow-container group bg-cta text-cta-text hover:bg-cta-hover inline-flex items-center gap-2 overflow-hidden rounded-md px-5 py-2.5 text-sm font-semibold shadow transition-all sm:text-base"
     >
       Download Resume
     </a>
